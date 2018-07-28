8277fa83b7249c487588f5d74e0d1d058f8719f2
David Laietta <davidjlaietta@gmail.com>
2018-07-28 06:56:14 -0600

    Adds sidebar-header file with active_sidebar

---
+++ sidebar-header.php
@@ -0,0 +1,5 @@
+<?php if ( is_active_sidebar( 'header-widget-area' ) ) : ?>
+	<div id="sidebar-header" class="sidebar-header">
+		<?php dynamic_sidebar( 'header-widget-area' ); ?>
+	</div>
+<?php endif; ?>
