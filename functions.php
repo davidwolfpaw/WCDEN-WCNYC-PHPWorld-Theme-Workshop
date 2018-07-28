99ed0f274f03e8376177cafc89cd85083aa72caa
David Laietta <davidjlaietta@gmail.com>
2018-07-28 06:47:03 -0600

    adds header widget area

--- functions.php
+++ functions.php
@@ -6,3 +6,18 @@ function denver_wp_enqueue_scripts() {
 	wp_enqueue_style( 'twentyseventeen-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'denver-child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
+
+
+// Add Header Widget Area
+add_action( 'widgets_init', 'denver_widgets_init' );
+function denver_widgets_init() {
+	register_sidebar(
+		array(
+			'id'            => 'header-widget-area',
+			'name'          => __( 'Header Widget Area', 'denver' ),
+			'description'   => __( 'Add widgets here to appear in your footer.', 'denver' ),
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+		)
+	);
+}
