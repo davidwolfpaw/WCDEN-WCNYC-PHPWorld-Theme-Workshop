bbfdc68c559de5c58a566dccec98d6266afb7e42
David Laietta <davidjlaietta@gmail.com>
2018-07-29 07:11:48 -0600

    updates styling to header sidebar

--- sidebar-header.php
+++ sidebar-header.php
@@ -1,5 +1,7 @@
 <?php if ( is_active_sidebar( 'header-widget-area' ) ) : ?>
-	<div id="sidebar-header" class="sidebar-header">
-		<?php dynamic_sidebar( 'header-widget-area' ); ?>
+	<div class="wrap">
+		<div id="sidebar-header" class="sidebar-header">
+			<?php dynamic_sidebar( 'header-widget-area' ); ?>
+		</div>
 	</div>
 <?php endif; ?>
