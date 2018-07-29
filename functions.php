0c02178ae950dc9b5914bfa4b212e5bc6d39faef
David Laietta <davidjlaietta@gmail.com>
2018-07-29 07:31:24 -0600

    adds our own section and setting to the customizer

--- functions.php
+++ functions.php
@@ -27,3 +27,31 @@ add_action( 'after_setup_theme', 'denver_after_setup_theme', 11 );
 function denver_after_setup_theme() {
 	register_nav_menu( 'header_secondary', __( 'Secondary Header Menu', 'denver' ) );
 }
+
+
+add_action( 'customize_register', 'denver_customize_register' );
+function denver_customize_register( WP_Customize_Manager $wp_customize ) {
+	$wp_customize->add_section(
+		'section_archives', array(
+			'title' => __( 'Archives', 'denver' ),
+		)
+	);
+	$wp_customize->add_setting(
+		'show_excerpts_in_archives', array(
+			'type'      => 'theme_mod',
+			'transport' => 'refresh',
+		)
+	);
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'show_excerpts_in_archives_control',
+			array(
+				'label'    => __( 'Only show excerpts on archive pages', 'denver' ),
+				'section'  => 'section_archives',
+				'settings' => 'show_excerpts_in_archives',
+				'type'     => 'checkbox',
+			)
+		)
+	);
+}
